064c7cf6c000fc0eaebf3032d7f7d29e477aa123
Ashim-das123 <adas85042@gmail.com>
2022-11-28 18:17:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let outputscreen = document.getElementById('output-screen');
+
+function display(num){
+outputscreen.value += num;
+}
+function calculate(){
+   try{
+      outputscreen.value = eval(outputscreen.value);
+   }
+   catch{
+      alert("invalid");
+   }
+}
+
+function alldelete() {
+   outputscreen.value = "";
+}
+function del(){
+  outputscreen.value = outputscreen.value.slice(0,-1);
+}
